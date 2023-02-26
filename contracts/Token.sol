8e04e417d5ea1df03379a495645850a0d8146c3e
Manish Roy <manish.roy@thirdpresence.com>
2023-02-26 19:40:52 +0530

    added methods for transfer and corresponding tests

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -9,7 +9,13 @@ contract Token {
     uint8 public decimals = 18;
     uint256 public totalSupply; 
 
-    mapping (address => uint256) public balanceOf;
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(
+        address indexed from,
+        address indexed to,
+        uint256 value
+    ); 
 
     constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
         name = _name;
@@ -17,6 +23,29 @@ contract Token {
         totalSupply = _totalSupply * (10 ** decimals);
 
         balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(_value < balanceOf[msg.sender], "Amount should not be greater than sender's balance.");
+        require(_to != address(0), "Cannot transfer to null account.");
+
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(msg.sender, _to, _value);
+        return true;
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+        require(_value < balanceOf[_from], "Amount should not be greater than sender's balance.");
+        require(_from != address(0), "Cannot transfer from null account.");
+        require(_to != address(0), "Cannot transfer to null account.");
+
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        emit Transfer(_from, _to, _value);
+        return true;
     }
 
 }
--- test/Token.js
+++ test/Token.js
@@ -5,15 +5,15 @@ const tokens = n => ethers.utils.parseUnits(n.toString(), 'ether')
 
 
 describe('Token', () => { 
-    let token, accounts, deployer;
+    let token, accounts, deployer, user1, user2;
     const [ NAME, SYMBOL, TOTAL_SUPPLY ]  = [ "Dex Token", "DXT", 2000000 ]
 
     beforeEach(async () => {
         const Token = await ethers.getContractFactory('Token')
         token = await Token.deploy(NAME, SYMBOL, TOTAL_SUPPLY)
 
-        accounts = await ethers.getSigners()
-        deployer = accounts[0]
+        accounts = await ethers.getSigners();
+        [ deployer, user1, user2 ] = accounts
     })
 
     describe("Deployment", () => {
@@ -36,5 +36,84 @@ describe('Token', () => {
         it('assigns total supply to deployer', async () => {
             expect(await token.balanceOf(deployer.address)).to.equal(tokens(TOTAL_SUPPLY))
         })
+    })
+
+    describe("Transfers", () => {
+        describe("From deployer to a user", () => {
+            let transaction, result;
+            
+            beforeEach(async () => {
+                transaction = await token.connect(deployer).transfer(user1.address, tokens(1))
+                result = await transaction.wait()
+            })
+    
+            it('transfers valid number of token/s', async () => {
+                expect(await token.balanceOf(deployer.address)).to.equal(tokens(TOTAL_SUPPLY - 1))
+                expect(await token.balanceOf(user1.address)).to.equal(tokens(1))
+            })
+    
+            it('emits a Transfer event', async () => {
+                const event = result.events[0]
+                expect(event.event).to.equal('Transfer')
+    
+                const eventArgs = event.args
+                expect(eventArgs.from).to.equal(deployer.address)
+                expect(eventArgs.to).to.equal(user1.address)
+                expect(eventArgs.value).to.equal(tokens(1))
+            })
+
+            it('rejects insufficient balance case', async () => {
+                const transaction = token.connect(deployer).transfer(user1.address, tokens(TOTAL_SUPPLY + 1))
+                await expect(transaction).to.be.reverted
+            })
+
+            it('rejects invalid recipient', async () => {
+                const transaction = token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', tokens(1))
+                await expect(transaction).to.be.reverted
+            })
+        })
+
+        describe("From one user to another", () => {
+            let transaction, result;
+            
+            beforeEach(async () => {
+                const tx = await token.connect(deployer).transfer(user1.address, tokens(10))
+                await tx.wait()
+                
+                transaction = await token.connect(deployer).transferFrom(user1.address, user2.address, tokens(1))
+                result = await transaction.wait()
+            })
+    
+            it('transfers valid number of token/s', async () => {
+                expect(await token.balanceOf(user1.address)).to.equal(tokens(10 - 1))
+                expect(await token.balanceOf(user2.address)).to.equal(tokens(1))
+            })
+    
+            it('emits a Transfer event', async () => {
+                const event = result.events[0]
+                expect(event.event).to.equal('Transfer')
+    
+                const eventArgs = event.args
+                expect(eventArgs.from).to.equal(user1.address)
+                expect(eventArgs.to).to.equal(user2.address)
+                expect(eventArgs.value).to.equal(tokens(1))
+            })
+
+            it('rejects insufficient balance case', async () => {
+                const transaction = token.connect(deployer).transferFrom(user1.address, user2.address, tokens(TOTAL_SUPPLY + 1))
+                await expect(transaction).to.be.reverted
+            })
+
+            it('rejects invalid sender', async () => {
+                const transaction = token.connect(deployer).transferFrom('0x0000000000000000000000000000000000000000', user2.address, tokens(1))
+                await expect(transaction).to.be.reverted
+            })
+
+            it('rejects invalid recipient', async () => {
+                const transaction = token.connect(deployer).transferFrom(user1.address, '0x0000000000000000000000000000000000000000', tokens(1))
+                await expect(transaction).to.be.reverted
+            })
+        })
+
     })
 })
