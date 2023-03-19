9300e6aca2e910ce2420f332ba9563b26a7f2339
Manish Roy <manish.11612939@lpu.in>
2023-03-20 01:47:31 +0530

    updated deploy script for latest changes

---
+++ scripts/deploy.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+const writeToFile = (data, chainId) => {
+  const result = {}
+  result[chainId] = {}
+
+  data.forEach(item => {
+    result[chainId][item[0]] = {"address": item[1]}
+  })
+
+  fs.writeFileSync('src/config.json', JSON.stringify(result));
+}
+
+async function main() {
+  const [ deployer, feeAccount ] = await ethers.getSigners()
+
+    const ExchangeC = await ethers.getContractFactory("Exchange")
+    const TokenC = await ethers.getContractFactory("Token")
+
+    const tokens = [
+      ["Dex Token", "DXT", 2000000],
+      ["Mocked DAI", "mDAI", 2000000],
+      ["Mocked ETH", "mETH", 2000000]
+    ]
+
+    const data = []
+
+    const ExchangeD = await ExchangeC.deploy(feeAccount.address, 10)
+    await ExchangeD.deployed()
+    console.log(`Exchange contract was deployed @ ${ ExchangeD.address }`)
+    data.push(["exchange", ExchangeD.address])
+
+    for await (let item of tokens) {
+      const [ _name, _sym, _supply ] = item
+      const TokenD = await TokenC.deploy(_name, _sym, _supply)
+      await TokenD.deployed()
+      console.log(`Token contract: ${ _name }(${ _sym }) was deployed @ ${ TokenD.address }`)
+      data.push([_sym, TokenD.address])
+    }
+
+    const { chainId } = await ethers.provider.getNetwork()
+    writeToFile(data, chainId);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
