af902b44c60d79f85a00ff19e22acd7255e62f6e
Manish Roy <manish.11612939@lpu.in>
2023-03-26 12:26:37 +0530

    initialised dirs and files for app and components

---
+++ src/app/App.js
@@ -0,0 +1,17 @@
+function App() {
+
+  return (
+    <div>
+      <main className='exchange grid'>
+        <section className='exchange__section--left grid'>
+
+        </section>
+        <section className='exchange__section--right grid'>
+
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
