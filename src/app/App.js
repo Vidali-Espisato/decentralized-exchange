9168c640e3a26a4470a551ea9f38dfb59a413327
Manish Roy <manish.11612939@lpu.in>
2023-03-26 12:28:25 +0530

    implemented initial loadBlockChainData function

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,31 @@
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import config from "../config.json"
+import exchangeAbi from "../abis/Exchange.json"
+
+
 function App() {
+  const [ accounts, setAccounts ] = useState([])
+
+  const loadBlockChainData = async () => {
+    const _accounts = await window.ethereum.request({method: "eth_requestAccounts"})
+    setAccounts(_accounts)
+
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    const { chainId } = await provider.getNetwork()
+
+    const exchange = new ethers.Contract(config[chainId].exchange.address, exchangeAbi, provider)
+    console.log(exchange.address)
+
+    const feePercent = await exchange.feePercent()
+    console.log(feePercent.toString())
+
+  }
+
+  useEffect(() => {
+    loadBlockChainData()
+  }, [])
+
 
   return (
     <div>
